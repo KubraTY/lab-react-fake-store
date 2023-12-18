9c3a906c68f1c0a60997e59650c331be5f45ed2d
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2023-12-18 18:13:43 +0100

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
+  const { productId } = useParams();
   const [product, setProduct] = useState({});
-
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+        const data = await response.json();
+        setProduct(data)
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProduct();
+  },[])
 
   // The `productId` coming from the URL parameter is available in the URL path.
   // You can access it with the `useParams` hook from react-router-dom.
@@ -14,10 +28,10 @@ function ProductDetailsPage() {
   // To fetch the product details, set up an effect with the `useEffect` hook:
 
 
-
   return (
     <div className="ProductDetailsPage">
     {/* Render product details here */}
+    <div>{product.title}</div>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,41 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import styles from "./ProductCard.module.css"
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
-  // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
-  // To fetch the list of products, set up an effect with the `useEffect` hook:
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
 
+    fetchProducts();
+  }, []);
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link to={`product/details/${product.id}`}  className={styles.productCard}>
+              <div className={styles.pic}><img src={product.image} alt="" /></div>
+              <div className={styles.title}>{product.title}</div>
+              <div className="category">{product.category}</div>
+              <div className="price">{product.price}</div>
+              <div className={styles.desc}>{product.description}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {console.log(products)}
     </div>
   );
 }
